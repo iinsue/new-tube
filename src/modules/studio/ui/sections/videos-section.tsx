657557d6fd1fce55f9fef2657ac6426229220ff6
iinsue <hinsu93@gmail.com>
2025-02-27 10:11:40 +0900

    Mux Webhook setup

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import Link from "next/link";
 import { Suspense } from "react";
+import { format } from "date-fns";
 import { trpc } from "@/trpc/client";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { DEFAULT_LIMIT } from "@/constants";
+import { snakeCaseToTitle } from "@/lib/utils";
+import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
+
 import { InfiniteScroll } from "@/components/infinite-scroll";
 import {
   Table,
@@ -14,7 +19,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import Link from "next/link";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 export const VideosSection = () => {
   return (
@@ -58,10 +63,49 @@ export const VideosSectionSuspense = () => {
                   legacyBehavior
                 >
                   <TableRow className="cursor-pointer">
-                    <TableCell>{video.title}</TableCell>
-                    <TableCell>visibility</TableCell>
-                    <TableCell>status</TableCell>
-                    <TableCell>date</TableCell>
+                    <TableCell>
+                      <div className="flex items-center gap-4">
+                        <div className="relative aspect-video w-36 shrink-0">
+                          <VideoThumbnail
+                            imageUrl={video.thumbnailUrl}
+                            previewUrl={video.previewUrl}
+                            duration={video.duration ?? 0}
+                            title={video.title}
+                          />
+                        </div>
+
+                        <div className="flex flex-col gap-y-1 overflow-hidden">
+                          <span className="line-clamp-1 text-sm">
+                            {video.title}
+                          </span>
+                          <span className="line-clamp-1 text-xs">
+                            {video.description ?? "No description"}
+                          </span>
+                        </div>
+                      </div>
+                    </TableCell>
+
+                    <TableCell>
+                      <div className="flex items-center">
+                        {video.visibility === "private" ? (
+                          <LockIcon className="mr-2 size-4" />
+                        ) : (
+                          <Globe2Icon className="mr-2 size-4" />
+                        )}
+                        {snakeCaseToTitle(video.visibility)}
+                      </div>
+                    </TableCell>
+
+                    <TableCell>
+                      <div className="flex items-center">
+                        {snakeCaseToTitle(video.muxStatus ?? "error")}
+                      </div>
+                    </TableCell>
+
+                    <TableCell className="truncate text-sm">
+                      {format(new Date(video.createdAt), "d MMM yyyy")}
+                    </TableCell>
+
                     <TableCell>views</TableCell>
                     <TableCell>comments</TableCell>
                     <TableCell>likes</TableCell>
---
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+import { formatDuration } from "@/lib/utils";
+
+interface VideoThumbnailProps {
+  title: string;
+  duration: number;
+  imageUrl?: string | null;
+  previewUrl?: string | null;
+}
+
+export const VideoThumbnail = ({
+  title,
+  duration,
+  imageUrl,
+  previewUrl,
+}: VideoThumbnailProps) => {
+  return (
+    <div className="group relative">
+      {/* Thumbnail wrapper */}
+      <div className="relative aspect-video w-full overflow-hidden rounded-xl">
+        <Image
+          src={imageUrl ?? "/placeholder.svg"}
+          alt={title}
+          fill
+          className="h-full w-full object-cover group-hover:opacity-0"
+        />
+
+        <Image
+          src={previewUrl ?? "/placeholder.svg"}
+          alt={title}
+          fill
+          className="h-full w-full object-cover opacity-0 group-hover:opacity-100"
+        />
+      </div>
+
+      {/* Video duration box */}
+      <div className="absolute bottom-2 right-2 rounded bg-black/80 px-1 py-0.5 text-xs font-medium text-white">
+        {formatDuration(duration)}
+      </div>
+    </div>
+  );
+};
