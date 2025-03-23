68eb88efba760f92d3832115dc6c82479375403d
iinsue <hinsu93@gmail.com>
2025-03-23 23:41:36 +0900

    video 상세 페이지 스켈레톤 추가

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -8,9 +8,32 @@ import { VideoOwner } from "./video-owner";
 import { VideoReactions } from "./video-reactions";
 import { VideoDescription } from "./video-description";
 
+import { Skeleton } from "@/components/ui/skeleton";
+
 interface VideoTopRowProps {
   video: VideoGetOneOutput;
 }
+
+export const VideoTopRowSkeleton = () => {
+  return (
+    <div className="mt-4 flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-6 w-4/5 md:w-2/5" />
+      </div>
+      <div className="flex w-full items-center justify-between">
+        <div className="flex w-[70%] items-center gap-3">
+          <Skeleton className="size-10 shrink-0 rounded-full" />
+          <div className="flex w-full flex-col gap-2">
+            <Skeleton className="h-5 w-4/5 md:w-1/3" />
+            <Skeleton className="h-5 w-3/5 md:w-1/5" />
+          </div>
+        </div>
+        <Skeleton className="h-9 w-1/3 rounded-full md:w-1/6" />
+      </div>
+      <div className="h-[120px] w-full" />
+    </div>
+  );
+};
 
 export const VideoTopRow = ({ video }: VideoTopRowProps) => {
   const compactViews = useMemo(() => {
