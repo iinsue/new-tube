93b0eec27f29298770674d31ba90c7ecf023553b
iinsue <hinsu93@gmail.com>
2025-03-30 23:44:28 +0900

    추천 영상 기능 구현

---
+++ src/modules/videos/ui/components/video-info.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import { useMemo } from "react";
+import { formatDistanceToNow } from "date-fns";
+
+import { UserAvatar } from "@/components/user-avatar";
+import { UserInfo } from "@/modules/users/ui/components/user-info";
+
+import { VideoGetManyOutput } from "../../types";
+import { VideoMenu } from "./video-menu";
+
+interface VideoInfoProps {
+  data: VideoGetManyOutput["items"][number];
+  onRemove?: () => void;
+}
+
+export const VideoInfo = ({ data, onRemove }: VideoInfoProps) => {
+  const compactViews = useMemo(() => {
+    return Intl.NumberFormat("en", {
+      notation: "compact",
+    }).format(data.viewCount);
+  }, [data.viewCount]);
+
+  const compactDate = useMemo(() => {
+    return formatDistanceToNow(data.createdAt, { addSuffix: true });
+  }, [data.createdAt]);
+
+  return (
+    <div className="flex gap-3">
+      <Link href={`/users/${data.user.id}`}>
+        <UserAvatar imageUrl={data.user.imageUrl} name={data.user.name} />
+      </Link>
+      <div className="min-w-0 flex-1">
+        <Link href={`/videos/${data.id}`}>
+          <h3 className="line-clamp-1 break-words text-base font-medium lg:line-clamp-2">
+            {data.title}
+          </h3>
+        </Link>
+
+        <Link href={`/users/${data.user.id}`}>
+          <UserInfo name={data.user.name} />
+        </Link>
+
+        <Link href={`/videos/${data.id}`}>
+          <p className="line-clamp-1 text-sm text-gray-600">
+            {compactViews} views * {compactDate}
+          </p>
+        </Link>
+      </div>
+
+      <div className="flex-shrink-0">
+        <VideoMenu videoId={data.id} onRemove={onRemove} />
+      </div>
+    </div>
+  );
+};
