2f99da86d547671be599684d532e70c517ba3657
insu <hinsu93@gmail.com>
2025-04-04 17:04:55 +0900

    공개 영상만 추천영상에 뜨도록 설정

--- src/modules/suggestions/server/procedures.ts
+++ src/modules/suggestions/server/procedures.ts
@@ -61,6 +61,7 @@ export const suggestionsRouter = createTRPCRouter({
         .where(
           and(
             not(eq(videos.id, existingVideo.id)),
+            eq(videos.visibility, "public"),
             existingVideo.categoryId
               ? eq(videos.categoryId, existingVideo.categoryId)
               : undefined,
