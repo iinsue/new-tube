d2c498c2a9085ba3612d8bb90f95d3135e811714
iinsue <hinsu93@gmail.com>
2025-04-06 22:44:08 +0900

    Playlist 프로시저 생성

---
+++ src/modules/playlists/server/procedures.ts
@@ -0,0 +1,94 @@
+import { z } from "zod";
+import { and, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
+
+import { db } from "@/db";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { users, videoReactions, videos, videoViews } from "@/db/schema";
+
+export const playlistRouter = createTRPCRouter({
+  getHistory: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            viewedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      const { id: userId } = ctx.user;
+      const { cursor, limit } = input;
+
+      const viewerVideoViews = db.$with("viewer_video_views").as(
+        db
+          .select({
+            videoId: videoViews.videoId,
+            viewedAt: videoViews.updatedAt,
+          })
+          .from(videoViews)
+          .where(eq(videoViews.userId, userId)),
+      );
+
+      const data = await db
+        .with(viewerVideoViews)
+        .select({
+          ...getTableColumns(videos),
+          user: users,
+          viewedAt: viewerVideoViews.viewedAt,
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          likeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "like"),
+            ),
+          ),
+          dislikeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "dislike"),
+            ),
+          ),
+        })
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .innerJoin(viewerVideoViews, eq(videos.id, viewerVideoViews.videoId))
+        .where(
+          and(
+            eq(videos.visibility, "public"),
+            cursor
+              ? or(
+                  lt(viewerVideoViews.viewedAt, cursor.viewedAt),
+                  and(
+                    eq(viewerVideoViews.viewedAt, cursor.viewedAt),
+                    lt(videos.id, cursor.id),
+                  ),
+                )
+              : undefined,
+          ),
+        )
+        .orderBy(desc(viewerVideoViews.viewedAt), desc(videos.id))
+        // Add 1 to the limit to check if there is more data
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit;
+
+      // Remove the last item if there is more data
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      // Set the next cursor to the last item if there is more data
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            viewedAt: lastItem.viewedAt,
+          }
+        : null;
+
+      return { items, nextCursor };
+    }),
+});
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,7 @@ import { searchRouter } from "@/modules/search/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { commentsRouter } from "@/modules/comments/server/procedures";
+import { playlistRouter } from "@/modules/playlists/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { suggestionsRouter } from "@/modules/suggestions/server/procedures";
@@ -16,6 +17,7 @@ export const appRouter = createTRPCRouter({
   studio: studioRouter,
   videos: videosRouter,
   comments: commentsRouter,
+  playlists: playlistRouter,
   categories: categoriesRouter,
   videoViews: videoViewsRouter,
   subscriptions: subscriptionsRouter,
